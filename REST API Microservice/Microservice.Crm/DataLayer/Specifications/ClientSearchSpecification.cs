c90f61eb8180d8b4f0bcf709b87d8450b336e08b
diamond_dragon <diamond_dragon@tut.by>
2020-05-03 21:36:46 +0300

    Migration added

---
+++ REST API Microservice/Microservice.Crm/DataLayer/Specifications/ClientSearchSpecification.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using GodelTech.Microservices.EntityFrameworkCore.Specifications;
+using Microservice.Crm.DataLayer.Entities;
+
+namespace Microservice.Crm.DataLayer.Specifications
+{
+    public class ClientSearchSpecification : Specification<Client>
+    {
+        public string FirstName { get; set; }
+
+        public override IQueryable<Client> AddPredicates(IQueryable<Client> query)
+        {
+            if (!string.IsNullOrWhiteSpace(FirstName))
+                query = query.Where(x => x.FirstName == FirstName);
+
+            return query;
+        }
+
+        public override IQueryable<Client> AddSorting(IQueryable<Client> query)
+        {
+            return query.OrderBy(x => x.Id);
+        }
+    }
+}
