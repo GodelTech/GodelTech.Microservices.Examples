de4c6f453dfa525ec3aadf48e2f4e713e594f52b
diamond_dragon <diamond_dragon@tut.by>
2020-05-03 21:48:55 +0300

    Commands added

--- REST API Microservice/Microservice.Crm/v1/Controllers/ClientController.cs
+++ REST API Microservice/Microservice.Crm/v1/Controllers/ClientController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using GodelTech.Microservices.Swagger.Swagger;
 using Microservice.Crm.v1.Contracts.Documents;
 using Microservice.Crm.v1.Contracts.Requests;
+using Microservice.Crm.v1.Resources.Clients;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -15,33 +16,40 @@ namespace Microservice.Crm.v1.Controllers
     [ApiController]
     public class ClientController : ControllerBase
     {
+        private readonly Lazy<IGetCommand> _getCommand;
+        private readonly Lazy<ICreateCommand> _createCommand;
+
+        public ClientController(Lazy<IGetCommand> getCommand, Lazy<ICreateCommand> createCommand)
+        {
+            _getCommand = getCommand ?? throw new ArgumentNullException(nameof(getCommand));
+            _createCommand = createCommand ?? throw new ArgumentNullException(nameof(createCommand));
+        }
+
         [Authorize(Startup.Policies.ClientsManage)]
         [SwaggerRequiredScopes(Startup.Scopes.ClientsManage)]
         [HttpPost]
         [ProducesResponseType(typeof(ClientDocument), StatusCodes.Status201Created)]
         [ProducesResponseType(typeof(IDictionary<string, string>), StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> CreateAsync(Guid jobId, [FromBody] CreateClientRequest request)
+        public async Task<IActionResult> CreateAsync([FromBody] CreateClientRequest request)
         {
             if (request == null)
                 throw new ArgumentNullException(nameof(request));
 
-            // TODO: Put your logic here
+            var document = await _createCommand.Value.ExecuteAsync(request);
 
             return Created(
-                "http://url.com",
-                new ClientDocument());
+                Url.Action(nameof(GetAsync), new { id = document.Id }),
+                document);
         }
 
         [Authorize(Startup.Policies.ClientsRead)]
         [SwaggerRequiredScopes(Startup.Scopes.ClientsRead)]
-        [HttpGet("{activityId}")]
+        [HttpGet("{id}")]
         [ProducesResponseType(typeof(ClientDocument), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(IDictionary<string, string>), StatusCodes.Status404NotFound)]
-        public async Task<ClientDocument> GetAsync(Guid jobId, Guid activityId)
+        public async Task<ClientDocument> GetAsync(int id)
         {
-            // TODO: Put your logic here
-
-            return new ClientDocument();
+            return await _getCommand.Value.ExecuteAsync(id);
         }
     }
 }
