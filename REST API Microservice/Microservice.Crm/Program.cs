81ad34b81e361e48de22f019ee43ff34fa3670ee
diamond_dragon <diamond_dragon@tut.by>
2020-05-04 20:09:07 +0300

    Third party dependencies removed

--- REST API Microservice/Microservice.Crm/Program.cs
+++ REST API Microservice/Microservice.Crm/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System.Collections.Generic;
+using Autofac.Extensions.DependencyInjection;
 using GodelTech.Microservices.Core;
+using GodelTech.Microservices.Core.Utils;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace Microservice.Crm
 {
@@ -11,10 +16,32 @@ namespace Microservice.Crm
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            MicroserviceHost.CreateHostBuilder<Startup>(args, new Dictionary<string, string>
-            {
-                ["DB_CONNECTION_STRING"] = "ConnectionStrings:Default",
-            });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureLogging((context, logging) =>
+                {
+                    // Lines below are required to exclude default .NET Core logging providers
+                    // These providers are excluded to make sure that logging format remains
+                    // the same as in .NET Framework microservices.
+                    logging.ClearProviders();
+                })
+                .UseSerilog((context, loggerConfiguration) =>
+                {
+                    loggerConfiguration.ReadFrom.Configuration(context.Configuration);
+                })
+                .ConfigureAppConfiguration((context, configuration) =>
+                {
+                    configuration.AddInMemoryCollectionFromEnvVariables(new Dictionary<string, string>
+                    {
+                        ["DB_CONNECTION_STRING"] = "ConnectionStrings:Default",
+                    });
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+        }
     }
 }
